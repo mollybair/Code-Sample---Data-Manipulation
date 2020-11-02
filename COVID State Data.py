b50d536f3435a35b68d51e2cde7a982ba03788da
mollybair <mollybair@users.noreply.github.com>
2020-11-01 22:47:25 -0600

    Create grouped bar chart

--- COVID State Data.py
+++ COVID State Data.py
@@ -39,13 +39,11 @@ def reshape(df, stubnames, j):
     -------
     df_reshaped : long pandas dataframe
     """
-    # df['id'] = df.index
     df_reshaped = pd.wide_to_long(df, stubnames = stubnames, i = ['STATE', 'RANK'], j = j,\
                                   sep = ' ')
-    # df_reshaped.drop(columns = ['id'], inplace = True)    # drop id column
     return df_reshaped
 
-def format_date(df, col_name):
+def format_date(series):
     """
     Parameters
     ----------
@@ -56,11 +54,12 @@ def format_date(df, col_name):
     df : pandas dataframe
         date col has been reformatted as MM/DD instead of MMDD
     """
-    for i in df[col_name]:
+    new_series = []
+    for i in series:
         i = str(i)
         new = i[0] + '/' + i[1:]
-        df.replace({int(i):new}, inplace = True)
-    return df
+        new_series.append(new)
+    return new_series
 
 def make_soup(url, headers):
     """
@@ -77,26 +76,6 @@ def make_soup(url, headers):
     response = requests.get(url, headers = headers)
     soup = BeautifulSoup(response.text, 'lxml')
     return soup
-
-def check_soup(soup, strings):
-    """
-    Parameters
-    ----------
-    soup : Beautiful Soup object
-        DESCRIPTION.
-    strings : list of strings
-        contains string that should be in the soup object and strings that
-        should not be in the soup object
-
-    Returns
-    -------
-    None.
-    """
-    for string in strings:
-        if string in soup.text:
-            print('Yes, this soup object contains the string "' + string + '"')
-        else:
-            print('No, this soup object does not contain the string "' + string + '"')
             
 def get_array(soup):
     """
@@ -166,75 +145,74 @@ def join(df1, df2, id_col, new_col):
         col.append(int(i))
     df_merged = df1
     df_merged[new_col] = col
-    # df_merged.reset_index(inplace = True)
     return df_merged
 
-def multi_scatter(df, x, y_list, colors, labels, title, ylabel, save_as):
+def grouped_bar(x, y1, y2, y3, lab1, lab2, lab3, xticks, title):
+    """
+    Parameters
+    ----------
+    x : x variable
+        variable bars are grouped by
+    y1 : first bar in grouping
+    y2 : second bar in grouping
+    y3 : third bar in grouping
+    lab1 : first bar label
+    lab2 : second bar label
+    lab3 : third bar label
+    xticks : labels for xticks (bar groupings)
+    title : plot title
+    """
+    ind = np.arange(len(x))  # x location for bar groups
+    width = 0.2
+    # create plot
     fig, ax = plt.subplots()
-    counter = 0
-    for y in y_list:
-        ax.plot(df[x], df[y], color = colors[counter],\
-                label = labels[counter], linewidth = 1)
-        counter += 1
-    plt.rcParams["font.family"] = "serif"
+    bar1 = ax.bar(ind - 0.2, y1, width, label = lab1, color = 'mediumseagreen')
+    bar2 = ax.bar(ind, y2, width, label = lab2, color = 'cornflowerblue')
+    bar2 = ax.bar(ind + 0.2, y3, width, label = lab3, color = 'goldenrod')
+    # set x ticks
+    ax.set_xticks(ind)
+    ax.set_xticklabels(xticks)
+    # add title
     ax.set_title(title)
     ax.title.set_weight('bold')
-    ax.title.set_size(14)
-    ax.title.set_position([.5, 1.05])
-    ax.set_ylabel(ylabel)
-    ylab = ax.yaxis.get_label()
-    ylab.set_size(10)
-    plt.xticks(rotation = 45)
+    ax.title.set_size(16)
+    ax.title.set_position([.5, 1])
+    # set spines
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_lw(1.5)
     ax.spines['left'].set_lw(1.5)
-    plt.legend()
+    # misc formatting and display
+    plt.legend()    
+    plt.rcParams["font.family"] = "serif"
     plt.tight_layout()
-    myfig = plt.gcf()
     plt.show()
-    myfig.savefig(save_as, dpi = 1000)
 
 def main():
-    ## load and manipulate data
+    ## load data
     df_cases = csv_df('COVID Case Data.csv', 23)
-    
     ## scrape and shape web data
     rank_soup = make_soup('https://www.multistate.us/issues/covid-19-state-reopening-guide',\
                            {'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'})
-    # check_soup(rank_soup, ['COVID-19 State Reopening Guide', 'Ratings', 'Methodology',\
-    #                      'Molly'])     # should print yes, yes, yes, no
     rank_array = get_array(rank_soup)
     colnames = ['RANK', 'STATE', 'Reopening Plan', 'SCORE', 'RANK', 'STATE',\
                 'Reopening Plan', 'SCORE']
     df_rank = array_to_df(rank_array, colnames, 4)
     df_rank = df_rank.drop(columns = 'Reopening Plan')
-
     ## merge df_cases and df_rank
     df = join(df_cases, df_rank, 'STATE', 'RANK')
     df = reshape(df, ['TOTAL CASES', 'BLACK CASES', 'HISPANIC CASES', 'WHITE CASES',\
-                      'BLACK IR', 'HISPANIC IR', 'WHITE IR'], 'DATE')
+                      'BLACK CI', 'HISPANIC CI', 'WHITE CI'], 'DATE')
     df.reset_index(inplace = True)
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    df['DATE'] = format_date(df['DATE'])
+    ## create visualization to show relationship between cumulative incidence and reopening
+    ## rank, for 2 most open states and 2 most closed states for most recent wave of data
+    open_state = df.at[df['RANK'].idxmin(), 'STATE']
+    closed_state = df.at[df['RANK'].idxmax(), 'STATE']
+    df_recent = df[(df['DATE'] == '9/22')]
+    df_recent = df_recent[(df_recent['STATE'] == open_state) | (df_recent['STATE'] == closed_state)]
+    grouped_bar([closed_state, open_state], df_recent['WHITE CI'], df_recent['BLACK CI'],\
+                df_recent['HISPANIC CI'], 'White', 'Black', 'Hispanic',\
+                    [closed_state, open_state],'Cumulative Incidence by Race')   
+main()
+        
